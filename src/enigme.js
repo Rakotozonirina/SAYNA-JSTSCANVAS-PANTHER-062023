41bff445465d4c8ed87b6b3a9415521181ec9a6a
Zo Nirina <tomzonirina@gmail.com>
2023-07-04 11:12:13 +0300

    I start the enigme functional

--- src/enigme.js
+++ src/enigme.js
@@ -6,3 +6,20 @@ descH1.addEventListener("mouseover", () => {
         keyframes: [{scale: 1.1 },{scale:1 }]
     });
 });
+
+const respons = {
+    enigmeOne : "SI JE SUIS FIDELE C’EST A CE TRONE PEU IMPORTE QUI MONTE DESSUS",
+    enigmeTwo : "OTHER RESPONSE"
+}
+
+
+function FirstRespond(){
+    let inputValue = document.getElementById("respond").value;
+    if(inputValue === respons.enigmeOne){
+        console.log(respons.enigmeOne);
+    }else if(inputValue === ""){
+        swal(respons.enigmeTwo);
+    }else{
+        swal("wrong response");
+    }
+}
